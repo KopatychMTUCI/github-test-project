e2ef41e9b7091d4c2f367c25fd73372748ac6f29
Nikolay Kolyshev <sword.of.destiny2001@mail.ru>
2020-09-27 14:22:00 +0300

    change logo & start label

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './mtuic_logo.webp';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Добро пожаловать в МТУСИ
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://mtuci.ru/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Заходите на наш сайт
         </a>
       </header>
     </div>
