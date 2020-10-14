b801d8e22618431df46523cc49aee7fd341d29b6
Nikolay Kolyshev <sword.of.destiny2001@mail.ru>
2020-10-14 13:33:52 +0300

    update start-page UI

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Добро пожаловать в МТУСИ
+          Добро пожаловать в Хогвартс
         </p>
         <a
           className="App-link"
